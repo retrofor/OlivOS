6ea4a31efe12c812793b510bdbb22384e791d45e
HsiangNianian <i@jyunko.cn>
2024-12-07 22:34:30 +0800

    feat: add CommandHandler class for command execution

--- scripts.py
+++ scripts.py
@@ -6,18 +6,39 @@
 # ]
 # ///
 
+from __future__ import annotations
+
 import os
 import sys
-import OlivOS
+import OlivOS  # noqa: F401
+from typing import Any, Awaitable, Callable, List, Optional
 
-command_list = sys.argv[1:]
 
-if command_list[0] == "build":
-    if sys.platform == "win32":
-        os.system("pyinstaller main.spec")
-    elif sys.platform == "linux":
-        os.system("pyinstaller main_linux.spec")
-    elif sys.platform == "darwin":
-        os.system("pyinstaller main_mac.spec")
-if command_list[0] == "start":
-    os.system("python main.py")
+class CommandHandler:
+    def __init__(self, command_prefix: str) -> None:
+        self.command_prefix = command_prefix
+
+    def __getattr__(self, item: str) -> Callable[..., Awaitable[Any]]:
+        return getattr(self, item, None)
+    
+    def execute(self, *args: Optional[List[str]]) -> None:
+        getattr(self, self.command_prefix, lambda *args: print("Invalid command"))(*args)
+
+    def build(self, *args: Optional[List[str]]) -> None:
+        platform_commands = {
+            "win32": "pyinstaller main.spec",
+            "linux": "pyinstaller main_linux.spec",
+            "darwin": "pyinstaller main_mac.spec",  
+        }
+        os.system(platform_commands.get(sys.platform, "echo Unsupported platform"))
+
+    def start(self, *args: Optional[List[str]]) -> None:
+        os.system("python main.py")
+
+if __name__ == "__main__":
+    command_list = sys.argv[1:]
+    command_prefix = command_list[0]
+    args = command_list[1:]
+    if command_list:
+        handle = CommandHandler(command_prefix=command_prefix)
+        handle.execute(args)
