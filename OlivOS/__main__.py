e8da44afc02c816e69a4f18fd99937e7c891de3f
HsiangNianian <i@jyunko.cn>
2024-12-07 15:47:12 +0800

    build: Update pyproject.toml and remove setup.py

---
+++ OlivOS/__main__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+'''
+_______________________    ________________
+__  __ \__  /____  _/_ |  / /_  __ \_  ___/
+_  / / /_  /  __  / __ | / /_  / / /____ \
+/ /_/ /_  /____/ /  __ |/ / / /_/ /____/ /
+\____/ /_____/___/  _____/  \____/ /____/
+
+@File      :   main.py
+@Author    :   lunzhiPenxil仑质
+@Contact   :   lunzhipenxil@gmail.com
+@License   :   AGPL
+@Copyright :   (C) 2020-2021, OlivOS-Team
+@Desc      :   None
+'''
+
+# here put the import lib
+
+import os
+
+import OlivOS
+
+def main():
+    if not os.path.exists('./conf'):
+        os.makedirs('./conf')
+        OlivOS.bootAPI.Entity(
+    basic_conf = './conf/basic.json'
+    ).start()
+
+if __name__ == '__main__':
+    main()
